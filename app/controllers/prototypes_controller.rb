ad69d4f6ad6213fd6c4e0da89519bcfd7078b54d
Kenyy0804 <kenyy0804@gmail.com>
2024-03-04 11:58:43 +0900

    編集画面の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -29,10 +29,10 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    if Prototype.update(prototype_params)
+    if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
